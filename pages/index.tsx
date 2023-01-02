38daeff934e4f3d4b73254e98eeddac1f45df58d
변민우 <yja05092@gmail.com>
2023-01-02 20:00:56 +0900

    Tour of Tailwind
    Test Drive part One

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,26 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-black">Carrot Market</h1>
+    <div className=" flex-col space-y-5 bg-slate-400 py-20 px-10">
+      <div className=" rounded-3xl bg-white p-6 shadow-xl">
+        <span className="text-3xl font-bold">Select Item</span>
+        <div className="my-2 flex justify-between">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-gray-500">Tooly Table</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="mt-2 flex justify-between border-t-2 border-dashed pt-2">
+          <span>Total</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="mx-auto mt-5 w-2/4 rounded-xl bg-blue-500 p-3 text-center text-white">Checkout</div>
+      </div>
+      <div className=" rounded-2xl bg-white p-10 shadow-xl"></div>
+      <div className=" rounded-2xl bg-white p-10 shadow-xl"></div>
+      <div className=" rounded-2xl bg-white p-10 shadow-xl"></div>
     </div>
   );
 };
