dffaa9249d4df9d11795deef50f1fe253de8eec0
변민우 <yja05092@gmail.com>
2023-01-02 20:26:16 +0900

    Tour of Tailwind
    Test Drive part Two

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,28 @@ const Home: NextPage = () => {
         </div>
         <div className="mx-auto mt-5 w-2/4 rounded-xl bg-blue-500 p-3 text-center text-white">Checkout</div>
       </div>
-      <div className=" rounded-2xl bg-white p-10 shadow-xl"></div>
+      <div className=" overflow-hidden rounded-2xl bg-white shadow-xl">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-2xl text-white">Profile</span>
+        </div>
+        <div className="relative -top-5 rounded-3xl bg-white p-6">
+          <div className="relative -top-16 flex items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-bold">340</span>
+            </div>
+            <div className="h-24 w-24 rounded-full bg-red-400" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-bold">$2,310</span>
+            </div>
+          </div>
+          <div className="relative -mt-10 -mb-5 flex flex-col items-center">
+            <span className="text-lg font-bold">Tony Molloy</span>
+            <span className="text-sm text-gray-500">New York, USA</span>
+          </div>
+        </div>
+      </div>
       <div className=" rounded-2xl bg-white p-10 shadow-xl"></div>
       <div className=" rounded-2xl bg-white p-10 shadow-xl"></div>
     </div>
