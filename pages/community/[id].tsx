3262240860c738aff9eff10dd6bcb51c140d2d18
변민우 <yja05092@gmail.com>
2023-01-12 21:37:28 +0900

    Database Setup
    Prisma Setup

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -75,7 +75,7 @@ const CommunityPostDetail: NextPage = () => {
         <div className="px-4">
           <TextArea name="description" placeholder="답변을 입력하세요." required />
           <button className="mt-2 w-full rounded-md border border-transparent bg-orange-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 ">
-            Reply
+            답변
           </button>
         </div>
       </div>
