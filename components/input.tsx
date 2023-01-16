dd8582d2d422c601f0410d2001f8d46bc6351546
변민우 <yja05092@gmail.com>
2023-01-16 17:17:50 +0900

    Refactoring
    Enter Form

--- components/input.tsx
+++ components/input.tsx
@@ -1,11 +1,22 @@
+import { UseFormRegisterReturn } from "react-hook-form";
+
 interface InputProps {
   label: string;
   name: string;
   kind?: "text" | "phone" | "price";
-  [key: string]: any;
+  type: string;
+  register: UseFormRegisterReturn;
+  required: boolean;
 }
 
-export default function Input({ label, name, kind = "text", ...rest }: InputProps) {
+export default function Input({
+  label,
+  name,
+  kind = "text",
+  type,
+  register,
+  required,
+}: InputProps) {
   return (
     <div>
       <label className="mb-1 block text-gray-700" htmlFor={name}>
@@ -16,7 +27,9 @@ export default function Input({ label, name, kind = "text", ...rest }: InputProp
         <div className="relative flex items-center  rounded-md shadow-md">
           <input
             id={name}
-            {...rest}
+            type={type}
+            required={required}
+            {...register}
             className="w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400
              transition-colors focus:border-orange-500 focus:outline-none focus:ring-orange-500"
           />
@@ -30,7 +43,9 @@ export default function Input({ label, name, kind = "text", ...rest }: InputProp
           </div>
           <input
             id={name}
-            {...rest}
+            type={type}
+            required={required}
+            {...register}
             className="w-full appearance-none rounded-md border border-gray-300 px-3 py-2 pl-7 placeholder-gray-400 
             transition-colors focus:border-orange-500 focus:outline-none focus:ring-orange-500"
           />
@@ -50,7 +65,9 @@ export default function Input({ label, name, kind = "text", ...rest }: InputProp
           </span>
           <input
             id={name}
-            {...rest}
+            type={type}
+            required={required}
+            {...register}
             className="w-full appearance-none rounded-md rounded-l-none border border-gray-300 px-3 py-2
              placeholder-gray-400 shadow-sm transition-colors focus:border-orange-500 focus:outline-none
               focus:ring-orange-500"
