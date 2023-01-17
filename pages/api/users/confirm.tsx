da21fb464ddb5785d38530676bcfef967fbacf7c
변민우 <yja05092@gmail.com>
2023-01-17 22:46:46 +0900

    Authentication
    Serverless Sessions

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,11 +1,27 @@
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
+import { withIronSessionApiRoute } from "iron-session/next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<ResponseType>) => {
+  // 핸들러를 withIronSessionApiRoute 함수로 감싸줬기 때문에 req.session을 확인할 수 있다.
+  console.log(req.session);
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!exists) res.status(404).end();
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
   res.status(200).end();
 };
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password:
+    "348905390534985doifgjoirjdiojfgiidfjklcvnblkncxvblknipjtgwpijtwirjtoijeoigjoighjfoighjoif",
+});
