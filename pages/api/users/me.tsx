0f2f978df9e012ba17f76a22fb48c6b9fb2765a6
변민우 <yja05092@gmail.com>
2023-01-19 15:00:00 +0900

    Authentication
    Profile Handler

--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -12,23 +12,19 @@ declare module "iron-session" {
 }
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<ResponseType>) => {
-  // 핸들러를 withIronSessionApiRoute 함수로 감싸줬기 때문에 req.session을 확인할 수 있다.
-  console.log(req.session);
-  const { token } = req.body;
-  const exists = await client.token.findUnique({
+  console.log(req.session.user);
+  const profile = await client.user.findUnique({
     where: {
-      payload: token,
+      id: req.session.user?.id,
     },
   });
-  if (!exists) return res.status(404).end();
-  req.session.user = {
-    id: exists.userId,
-  };
-  await req.session.save();
-  res.status(200).end();
+  res.json({
+    ok: true,
+    profile,
+  });
 };
 
-export default withIronSessionApiRoute(withHandler("POST", handler), {
+export default withIronSessionApiRoute(withHandler("GET", handler), {
   cookieName: "carrotsession",
   password:
     "348905390534985doifgjoirjdiojfgiidfjklcvnblkncxvblknipjtgwpijtwirjtoijeoigjoighjfoighjoif",
