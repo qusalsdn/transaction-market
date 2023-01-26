fc8eeb84eae5506b08b8593403e5882352da7d3b
변민우 <yja05092@gmail.com>
2023-01-26 16:08:06 +0900

    동네생활
    Answer

---
+++ pages/api/posts/[id]/answers.ts
@@ -0,0 +1,33 @@
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<ResponseType>) => {
+  const {
+    query: { id },
+    session: { user },
+    body: { answer },
+  } = req;
+  const newAnswer = await client.answer.create({
+    data: {
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+      post: {
+        connect: {
+          id: Number(id),
+        },
+      },
+      answer,
+    },
+  });
+  res.status(200).json({
+    ok: true,
+    answer: newAnswer,
+  });
+};
+
+export default withApiSession(withHandler({ methods: ["POST"], handler }));
--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -8,6 +8,7 @@ import { Answer, Post, User } from "@prisma/client";
 import Link from "next/link";
 import useMutation from "@libs/client/useMutation";
 import { cls } from "@libs/client/utils";
+import { useForm } from "react-hook-form";
 
 interface AnswerWithUser extends Answer {
   user: User;
@@ -28,12 +29,24 @@ interface CommunityPostResponse {
   isWondering: boolean;
 }
 
+interface AnswerForm {
+  answer: string;
+}
+
+interface AnswerResponse {
+  ok: boolean;
+  response: Answer;
+}
+
 const CommunityPostDetail: NextPage = () => {
   const router = useRouter();
   const { data, mutate } = useSWR<CommunityPostResponse>(
     router.query.id ? `/api/posts/${router.query.id}` : null
   );
-  const [wonder] = useMutation(`/api/posts/${router.query.id}/wonder`);
+  const [wonder, { loading }] = useMutation(`/api/posts/${router.query.id}/wonder`);
+  const [sendAnswer, { data: answerData, loading: answerLoading }] =
+    useMutation<AnswerResponse>(`/api/posts/${router.query.id}/answers`);
+  const { register, handleSubmit, reset } = useForm<AnswerForm>();
 
   useEffect(() => {
     if (data && !data?.ok) {
@@ -59,8 +72,22 @@ const CommunityPostDetail: NextPage = () => {
       },
       false
     );
-    wonder({});
+    // 무한 클릭으로 인한 데이터 병목현상 방지
+    if (!loading) {
+      wonder({});
+    }
   };
+
+  const onValid = (data: AnswerForm) => {
+    if (answerLoading) return;
+    sendAnswer(data);
+  };
+
+  useEffect(() => {
+    if (answerData && answerData.ok) {
+      reset();
+    }
+  }, [answerData, reset]);
 
   return (
     <Layout canGoBack>
@@ -72,7 +99,7 @@ const CommunityPostDetail: NextPage = () => {
         <div className="flex cursor-pointer items-center space-x-3 px-4 pb-3">
           <div className="h-10 w-10 rounded-full bg-slate-300" />
           <div>
-            <p className="text-sm font-medium text-gray-700">{data?.post.user.name}</p>
+            <p className="text-sm font-medium text-gray-700">{data?.post?.user?.name}</p>
             <Link href={`/users/profiles/${data?.post?.user?.id}`}>
               <p className="text-xs font-medium text-gray-500">프로필 &rarr;</p>
             </Link>
@@ -82,7 +109,7 @@ const CommunityPostDetail: NextPage = () => {
         <div>
           <div className="mt-2 px-4 text-gray-700">
             <span className="font-medium text-orange-500">Q </span>
-            <span>{data?.post.question}</span>
+            <span>{data?.post?.question}</span>
           </div>
           <div className="mt-1 flex w-full space-x-5 border-b-[1px] px-4 py-2.5  text-gray-700">
             <button
@@ -103,7 +130,7 @@ const CommunityPostDetail: NextPage = () => {
                   d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                 ></path>
               </svg>
-              <span>궁금해요 {data?.post._count.wondering}</span>
+              <span>궁금해요 {data?.post?._count?.wondering}</span>
             </button>
             <span className="flex items-center space-x-2 text-sm">
               <svg
@@ -120,13 +147,13 @@ const CommunityPostDetail: NextPage = () => {
                   d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                 ></path>
               </svg>
-              <span>답변 {data?.post._count.answers}</span>
+              <span>답변 {data?.post?._count?.answers}</span>
             </span>
           </div>
         </div>
 
         <div className="my-5 space-y-5 px-4">
-          {data?.post.answers.map((answer) => (
+          {data?.post?.answers.map((answer) => (
             <div key={answer.id} className="flex items-start space-x-3">
               <div className="h-8 w-8 rounded-full bg-slate-200" />
               <div>
@@ -134,18 +161,23 @@ const CommunityPostDetail: NextPage = () => {
                   {answer.user.name}
                 </span>
                 <span className="block text-xs text-gray-500 ">{answer.createdAt}</span>
-                <p className="mt-2 text-gray-700">{}</p>
+                <p className="mt-2 text-gray-700">{answer.answer}</p>
               </div>
             </div>
           ))}
         </div>
 
-        <div className="px-4">
-          <TextArea name="description" placeholder="답변을 입력하세요." required />
+        <form className="px-4" onSubmit={handleSubmit(onValid)}>
+          <TextArea
+            name="description"
+            placeholder="답변을 입력하세요."
+            required
+            register={register("answer", { required: true, minLength: 5 })}
+          />
           <button className="mt-2 w-full rounded-md border border-transparent bg-orange-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 ">
-            답변
+            {answerLoading ? "로딩중..." : "답변"}
           </button>
-        </div>
+        </form>
       </div>
     </Layout>
   );
