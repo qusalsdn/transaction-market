982ad5b62cce4bf72472d04f185f3aa6f6a6ebec
변민우 <yja05092@gmail.com>
2023-01-27 16:32:23 +0900

    Profile
    Reviews

---
+++ pages/api/reviews.ts
@@ -0,0 +1,30 @@
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<ResponseType>) => {
+  const {
+    session: { user },
+  } = req;
+  const reviews = await client.review.findMany({
+    where: {
+      createForId: user?.id,
+    },
+    include: {
+      createBy: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  res.status(200).json({
+    ok: true,
+    reviews,
+  });
+};
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
