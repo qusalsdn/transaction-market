9e6edaeae3892abac98790605df8c95f0a3cdb47
변민우 <yja05092@gmail.com>
2023-01-30 20:57:15 +0900

    Streams
    Seeding

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+async function main() {
+  [...Array.from(Array(500).keys())].forEach(async (item) => {
+    const stream = await client.stream.create({
+      data: {
+        name: String(item),
+        price: item,
+        description: String(item),
+        user: {
+          connect: {
+            id: 1,
+          },
+        },
+      },
+    });
+    console.log(`${item}/500`);
+  });
+}
+
+// 데이터베이스를 시드하려면 db seed CLI 명령을 실행하십시오. npx prisma db seed
+main()
+  .catch((e) => console.log(e))
+  .finally(() => client.$disconnect());
