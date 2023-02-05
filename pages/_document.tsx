ed21c8fe6f7ce7cbbf1f1320869a05c7b5e179c5
변민우 <yja05092@gmail.com>
2023-02-05 18:22:14 +0900

    NextJs Deep Dive
    _document and Fonts

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+// __app.tsx는 브라우저 실행되지만 해당 파일은 서버에서 한 번만 실행된다.
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
