756eb84dd2e49c6f7f28e0e794b91ed19627aa72
변민우 <yja05092@gmail.com>
2023-02-05 14:59:53 +0900

    NextJs Deep Dive
    Middlewares

---
+++ middleware.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextFetchEvent } from "next/server";
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  console.log(req.cookies);
+}
+
+export const config = {};
