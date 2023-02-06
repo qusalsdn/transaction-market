8e82d4cfb494ba49382d9d40e29e3ed3f7da48c9
변민우 <yja05092@gmail.com>
2023-02-06 20:30:56 +0900

    NextJs Deep Dive
    Dynamic getStaticProps

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,8 +1,12 @@
 import { readdirSync } from "fs";
-import { NextPage } from "next";
+import matter from "gray-matter";
+import { GetStaticProps, NextPage } from "next";
+import remarkHtml from "remark-html";
+import remarkParse from "remark-parse/lib";
+import { unified } from "unified";
 
-const Post: NextPage = () => {
-  return <h1>hi</h1>;
+const Post: NextPage<{ post: string }> = ({ post }) => {
+  return <h1>{post}</h1>;
 };
 
 // getStaticPaths는 동적인 URL을 갖는 페이지에서 getStaticProps를 사용할 때 필요하다.
@@ -17,10 +21,12 @@ export function getStaticPaths() {
   };
 }
 
-export function getStaticProps() {
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { content } = matter.read(`./posts/${ctx.params?.slug}.md`);
+  const { value } = await unified().use(remarkParse).use(remarkHtml).process(content);
   return {
-    props: {},
+    props: { post: value },
   };
-}
+};
 
 export default Post;
