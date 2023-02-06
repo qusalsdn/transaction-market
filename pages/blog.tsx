1c48aa0b0578043da1b5750132051a17d0899255
변민우 <yja05092@gmail.com>
2023-02-06 17:29:33 +0900

    NextJs Deep Dive
    getStaticProps part One
    md 파일에서 --- 이 부분은 front matter라고 하고 이건 마크다운 파일에 관련된 데이터를 제공해준다.

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,14 +1,43 @@
 import Layout from "@components/layout";
+import { readdirSync, readFileSync } from "fs";
+import matter from "gray-matter";
+import { NextPage } from "next";
 
-const Blog = () => {
+interface Post {
+  title: string;
+  date: string;
+  category: string;
+}
+
+const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
   return (
     <Layout title="블로그" seoTitle="블로그">
-      <h1 className="textlg font-bold">최근 게시물</h1>
-      <ul>
-        <li>환영합니다!</li>
-      </ul>
+      <h1 className="textlg text-center font-bold">최근 게시물</h1>
+      {posts.map((post, index) => {
+        return (
+          <div key={index} className="my-2">
+            <span className="text-lg text-red-500">{post.title}</span>
+            <div>
+              <span>
+                {post.date} / {post.category}
+              </span>
+            </div>
+          </div>
+        );
+      })}
     </Layout>
   );
 };
+
+// getStaticProps는 딱 한 번만 실행된다.
+export async function getStaticProps() {
+  const blogPosts = readdirSync("./posts").map((file) => {
+    const content = readFileSync(`./posts/${file}`, "utf-8");
+    return matter(content).data;
+  });
+  return {
+    props: { posts: blogPosts },
+  };
+}
 
 export default Blog;
