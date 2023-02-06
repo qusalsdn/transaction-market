cd64007259abbd34dca5cb427e49bacb6ca5e615
변민우 <yja05092@gmail.com>
2023-02-06 16:41:45 +0900

    NextJs Deep Dive
    Blog Section

---
+++ pages/blog.tsx
@@ -0,0 +1,14 @@
+import Layout from "@components/layout";
+
+const Blog = () => {
+  return (
+    <Layout title="블로그" seoTitle="블로그">
+      <h1 className="textlg font-bold">최근 게시물</h1>
+      <ul>
+        <li>환영합니다!</li>
+      </ul>
+    </Layout>
+  );
+};
+
+export default Blog;
