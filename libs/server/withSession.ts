0852f830e641629f40e5283b84316c766ecaa72e
변민우 <yja05092@gmail.com>
2023-02-06 16:21:25 +0900

    NextJs Deep Dive
    SSR + Authentication

--- libs/server/withSession.ts
+++ libs/server/withSession.ts
@@ -1,4 +1,4 @@
-import { withIronSessionApiRoute } from "iron-session/next";
+import { withIronSessionApiRoute, withIronSessionSsr } from "iron-session/next";
 
 declare module "iron-session" {
   interface IronSessionData {
@@ -15,4 +15,9 @@ const cookieOptions = {
 
 export const withApiSession = (fn: any) => {
   return withIronSessionApiRoute(fn, cookieOptions);
+};
+
+// 위의 헬퍼 함수와 똑같은 기능이지만 getServerSideProps 안에서 인증 기능을 사용할 수 있게 해준다.
+export const withSsrSession = (handler: any) => {
+  return withIronSessionSsr(handler, cookieOptions);
 };
