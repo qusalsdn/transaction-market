4f88a65c3c8c42efab89eff39f0556fd38906382
변민우 <yja05092@gmail.com>
2023-02-06 18:47:34 +0900

    NextJs Deep Dive
    getStaticPaths part Three

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,8 +1,21 @@
+import { readdirSync } from "fs";
 import { NextPage } from "next";
 
 const Post: NextPage = () => {
   return <h1>hi</h1>;
 };
+
+// getStaticPaths는 동적인 URL을 갖는 페이지에서 getStaticProps를 사용할 때 필요하다.
+export function getStaticPaths() {
+  const files = readdirSync("./posts").map((file) => {
+    const [name, extension] = file.split(".");
+    return { params: { slug: name } };
+  });
+  return {
+    paths: files,
+    fallback: false,
+  };
+}
 
 export function getStaticProps() {
   return {
