a51536d21b2bd9968df75a07292bda0010ca6a14
변민우 <yja05092@gmail.com>
2023-02-21 18:15:29 +0900

    스트리밍이 끝난 후에는 라이브 채팅을 칠 수 없게 수정

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -186,23 +186,25 @@ const SelectStream: NextPage = () => {
                 />
               ))}
             </div>
-            <div className="fixed inset-x-0 bottom-0  bg-white py-2">
-              <form
-                onSubmit={handleSubmit(onValid)}
-                className="relative mx-auto flex w-full  max-w-md items-center"
-              >
-                <input
-                  type="text"
-                  {...register("message", { required: true })}
-                  className="w-full rounded-full border-gray-300 pr-12 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
-                />
-                <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
-                  <button className="flex items-center rounded-full bg-orange-500 px-3 text-sm text-white hover:bg-orange-600 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
-                    &rarr;
-                  </button>
-                </div>
-              </form>
-            </div>
+            {!data?.stream.completed && (
+              <div className="fixed inset-x-0 bottom-0  bg-white py-2">
+                <form
+                  onSubmit={handleSubmit(onValid)}
+                  className="relative mx-auto flex w-full  max-w-md items-center"
+                >
+                  <input
+                    type="text"
+                    {...register("message", { required: true })}
+                    className="w-full rounded-full border-gray-300 pr-12 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
+                  />
+                  <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
+                    <button className="flex items-center rounded-full bg-orange-500 px-3 text-sm text-white hover:bg-orange-600 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
+                      &rarr;
+                    </button>
+                  </div>
+                </form>
+              </div>
+            )}
           </div>
         </div>
       )}
